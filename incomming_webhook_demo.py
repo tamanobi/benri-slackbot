40ca92f80a59c32b52f56dd6636bd67479c6722d
tamanobi <tamanobi@gmail.com>
2020-03-27 20:32:39 +0900

    dotenvから読み込むようにした

--- incomming_webhook_demo.py
+++ incomming_webhook_demo.py
@@ -1,9 +1,11 @@
 import requests
 import json
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 endpoint = os.environ['SLACK_WEBHOOK']
-payload = {"text":"Slack Appsのテストです。環境変数を使うべき！"}
+payload = {"text":".envから読み込める"}
 res = requests.post(endpoint,data=json.dumps(payload))
 
 if res.status_code == 200:
