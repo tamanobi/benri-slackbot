3154771e90dccf05316577ad3a14d4d03f7cae96
tamanobi <tamanobi@gmail.com>
2020-03-27 22:27:08 +0900

    メンションをし返すボットになった

--- app.py
+++ app.py
@@ -21,12 +21,17 @@ def hear():
 
     if 'event' in request.json:
         event = request.json['event']
+        print(event)
         bot_id = event.get('bot_id')
         if bot_id is not None and bot_id == 'B010WR2FE2C':
             return jsonify({})
-
         endpoint = os.environ['SLACK_WEBHOOK']
         text = request.json['event']['text']
+
+        user = event['user']
+        # メンションをし返す
+        text = text.replace('<@U010KB4S65R>', f"<@{user}>")
+
         payload = {"text":text}
         res = requests.post(endpoint,data=json.dumps(payload))
 
