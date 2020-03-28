0d2849ce04170633aafb6056f0f2eaa53d997fe4
tamanobi <tamanobi@gmail.com>
2020-03-28 20:59:34 +0900

    blackによるフォーマット

--- index.py
+++ index.py
@@ -9,38 +9,40 @@ import random
 load_dotenv()
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def root_post():
     print(request)
-    challenge_token = request.json['challenge']
+    challenge_token = request.json["challenge"]
     return jsonify(challenge=challenge_token)
 
-@app.route('/listening', methods=['GET', 'POST'])
+
+@app.route("/listening", methods=["GET", "POST"])
 def hear():
-    if 'challenge' in request.json:
-        challenge_token = request.json['challenge']
+    if "challenge" in request.json:
+        challenge_token = request.json["challenge"]
         return jsonify(challenge=challenge_token)
 
-    if 'event' in request.json:
-        event = request.json['event']
+    if "event" in request.json:
+        event = request.json["event"]
         print(event)
-        bot_id = event.get('bot_id')
-        if bot_id is not None and bot_id == 'B010WR2FE2C':
+        bot_id = event.get("bot_id")
+        if bot_id is not None and bot_id == "B010WR2FE2C":
             return jsonify({})
-        endpoint = os.environ['SLACK_WEBHOOK']
-        if 'text' not in request.json['event']:
+        endpoint = os.environ["SLACK_WEBHOOK"]
+        if "text" not in request.json["event"]:
             return jsonify({})
 
-        text = request.json['event']['text']
+        text = request.json["event"]["text"]
 
-        user = event['user']
+        user = event["user"]
         RSS_URL = "https://b.hatena.ne.jp/hotentry/it.rss"
 
         d = feedparser.parse(RSS_URL)
-        text = "\n".join([
-            f"{entry.title}: {entry.link}" for entry in random.sample(d.entries, 3)
-        ])
+        text = "\n".join(
+            [f"{entry.title}: {entry.link}" for entry in random.sample(d.entries, 3)]
+        )
         payload = {"text": f"<@{user}> {text}"}
-        res = requests.post(endpoint,data=json.dumps(payload))
+        res = requests.post(endpoint, data=json.dumps(payload))
 
     return jsonify({})
